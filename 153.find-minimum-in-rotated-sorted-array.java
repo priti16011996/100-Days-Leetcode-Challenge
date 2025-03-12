93da4b1407267897157e8b18109bdc97d5c0e3d6
ECommerce <priti16maurya@gmail.com>
2025-03-12 23:58:11 +0530

    day 19 leetcode challenges

---
+++ 153.find-minimum-in-rotated-sorted-array.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=153 lang=java
+ *
+ * [153] Find Minimum in Rotated Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public int findMin(int[] nums) {
+        if(nums.length == 1)
+        {
+            return nums[0];
+        }
+        else if(nums[0]<nums[nums.length-1])
+        {
+            return nums[0];
+        }
+        int start=0;
+        int end = nums.length-1;
+        while(start<=end)
+        {
+            int mid = (start+end)/2;
+            if(mid !=0 && nums[mid-1]>nums[mid])
+            {
+                return nums[mid];
+            }
+            else if((mid != nums.length-1) && nums[mid]>nums[mid+1])
+            {
+                return nums[mid+1];
+            }
+            else if(nums[start] <= nums[mid])
+            {
+                start = mid+1;
+            }
+            else
+            {
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+}
+// @lc code=end
+
