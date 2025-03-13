2ca443ef884d9305296a6092b73cfdb04648060a
ECommerce <priti16maurya@gmail.com>
2025-03-13 23:52:24 +0530

    day 20 for leetcode challenge

---
+++ 162.find-peak-element.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=162 lang=java
+ *
+ * [162] Find Peak Element
+ */
+
+// @lc code=start
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left; 
+    }
+}
+// @lc code=end
+
